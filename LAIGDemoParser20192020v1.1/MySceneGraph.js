a518703259687b70efb7ade2f3d2932cf7a3ddfc
jorpac <36743869+jorpac@users.noreply.github.com>
2019-09-30 11:21:59 +0100

    Cylinder Working

--- LAIGDemoParser20192020v1.1/MySceneGraph.js
+++ LAIGDemoParser20192020v1.1/MySceneGraph.js
@@ -591,6 +591,25 @@ class MySceneGraph {
                 this.primitives[primitiveId] = cyl;
 
             }
+            else if(primitiveType == 'sphere') {
+                //radius
+                var radius = this.reader.getFloat(grandChildren[0], 'radius');
+                if (!(radius != null && !isNaN(radius)))
+                    return "unable to parse base of the primitive coordinates for ID = " + primitiveId;
+                //slices
+                var slices = this.reader.getFloat(grandChildren[0], 'slices');
+                if (!(slices != null && !isNaN(slices)))
+                    return "unable to parse base of the primitive coordinates for ID = " + primitiveId;
+                //base
+                var stacks = this.reader.getFloat(grandChildren[0], 'stacks');
+                if (!(stacks != null && !isNaN(stacks)))
+                    return "unable to parse base of the primitive coordinates for ID = " + primitiveId;
+
+                var sphere = new MySphere(this.scene, primitiveId, radius, slices, stacks);
+
+                this.primitives[primitiveId] = sphere;
+
+            }
             else {
                 console.warn("To do: Parse other primitives.");
             }
--- LAIGDemoParser20192020v1.1/MySphere.js
+++ LAIGDemoParser20192020v1.1/MySphere.js
@@ -2,13 +2,11 @@
 * MyCylinder
 * @constructor
 */
-class MyCylinder extends CGFobject {
-    constructor(scene, id, base, top, height, slices, stacks) {
+class MySphere extends CGFobject {
+    constructor(scene, id, radius, slices, stacks) {
         super(scene);
-        this.base = base;
-        this.top = top;
+        this.radius = radius;
         this.slices = slices;
-        this.height = height;
         this.stacks = stacks;
         this.initBuffers();
     }
@@ -21,29 +19,26 @@ class MyCylinder extends CGFobject {
         var ang = 0;
         var alphaAng = 2*Math.PI/this.slices;
 
-        var difRadius = this.top -this.base;
-        var stackRadius = difRadius/this.stacks;
-        //var tmpRadius = this.base;
+      
         for(var j = 0; j < this.stacks; j++){
 
             for(var i = 0; i < this.slices; i++){
 
-                var sa = Math.sin(ang) * this.base;
-                var saa = Math.sin(ang+alphaAng) * this.base;
-                var ca = Math.cos(ang) * this.base;
-                var caa = Math.cos(ang+alphaAng) * this.base;
-
-                var sa2 = Math.sin(ang) * (this.base+ stackRadius);
-                var saa2 = Math.sin(ang+alphaAng) * (this.base+ stackRadius);
-                var ca2 = Math.cos(ang) * (this.base+ stackRadius);
-                var caa2 = Math.cos(ang+alphaAng) * (this.base+ stackRadius);
+                var sa = Math.sin(ang) * this.radius;
+                var saa = Math.sin(ang+alphaAng) * this.radius;
+                var ca = Math.cos(ang) * this.radius;
+                var caa = Math.cos(ang+alphaAng) * this.radius;
 
                 // vertices of the face of the prism
-                this.vertices.push(ca2, sa2, (this.height/this.stacks)*(j+1));
+                this.vertices.push(ca, sa, (this.height/this.stacks)*(j+1));
                 this.vertices.push(ca, sa, j*(this.height/this.stacks));
-                this.vertices.push(caa2, saa2, (this.height/this.stacks)*(j+1));
+                this.vertices.push(caa, saa, (this.height/this.stacks)*(j+1));
                 this.vertices.push(caa, saa, j*(this.height/this.stacks));
-                
+                /*this.vertices.push(ca, sa, tempHeight + stackHeight);
+                this.vertices.push(ca, sa, tempHeight);
+                this.vertices.push(caa, saa, tempHeight + stackHeight);
+                this.vertices.push(caa, saa, tempHeight);*/
+
                 // normals of the face of the cylinder
                 this.normals.push(Math.cos(ang), 0, Math.sin(ang));
                 this.normals.push(Math.cos(ang), 0, Math.sin(ang));
@@ -51,6 +46,10 @@ class MyCylinder extends CGFobject {
                 this.normals.push(Math.cos(ang + alphaAng), 0, Math.sin(ang + alphaAng));
 
                 // indices of the face of the cylinder
+                /*this.indices.push(4*i+j*this.stacks, (4*i)+j*this.stacks+1 , (4*i)+j*this.stacks+2);
+                this.indices.push((4*i)+j*this.stacks + 1, (4*i)+j*this.stacks+3, (4*i)+j*this.stacks+2);
+                this.indices.push((4*i)+j*this.stacks + 2, (4*i)+j*this.stacks+1, 4*i+j*this.stacks);
+                this.indices.push((4*i)+j*this.stacks + 2, (4*i)+j*this.stacks+3, (4*i+j*this.stacks+1));*/
                 this.indices.push(4 * i + (4* (j*this.slices)), (4 * i + 1) + (4* (j*this.slices)), (4 * i + 2) + (4* (j*this.slices)));
                 this.indices.push((4 * i + 1) + (4* (j*this.slices)), (4 * i + 3) + (4* (j*this.slices)), (4 * i + 2) + (4* (j*this.slices)));
                 this.indices.push((4 * i + 2) + (4* (j*this.slices)), (4 * i + 1) + (4* (j*this.slices)), 4 * i + (4* (j*this.slices)));
@@ -66,7 +65,6 @@ class MyCylinder extends CGFobject {
 
                 ang+=alphaAng;
             }
-            this.base+=stackRadius;
             ang = 0;
     }
 
