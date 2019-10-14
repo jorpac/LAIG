534044fe3925f1546e087022d5fe0be0d9fb1927
jorpac <36743869+jorpac@users.noreply.github.com>
2019-10-14 15:57:37 +0100

    Textures updated, texCoords for MySphere

--- LAIGDemoParser20192020v1.1/MySphere.js
+++ LAIGDemoParser20192020v1.1/MySphere.js
@@ -20,12 +20,22 @@ class MySphere extends CGFobject {
         var nver = 0;
         var betaInc = (Math.PI / 2) / this.stacks;
 
+
+        var v_tex = 1/(this.stacks * 2);
+        var u_tex = 1/this.slices;
+        var v_temp = 1;
+        var u_temp = 0;
+
         for (var alpha = 0; alpha <= 2 * Math.PI; alpha += alphaInc) {
+            v_temp = 1;
             for (var beta = -Math.PI / 2; beta <= Math.PI / 2; beta += betaInc) {
                 this.vertices.push(this.radius * Math.cos(beta) * Math.cos(alpha), this.radius * Math.cos(beta) * Math.sin(alpha), this.radius * Math.sin(beta));
                 this.normals.push(Math.cos(beta) * Math.cos(alpha), Math.cos(beta) * Math.sin(alpha), Math.sin(beta));
+                this.texCoords.push(u_temp, v_temp);
                 nver++;
+                v_temp-=v_tex;
             }
+            u_temp+= u_tex;
         }
 
         for (var i = 0; i < nver - (2 * this.stacks + 1); i++) {
