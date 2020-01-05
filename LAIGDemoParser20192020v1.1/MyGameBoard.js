e4894a9f65e37e7bf9a0675d9cfc4e46fda85373
Vasco Teixeira <vmlteixeira_20@hotmail.com>
2020-01-05 19:43:19 +0000

    cuts

--- LAIGDemoParser20192020v1.1/MyGameBoard.js
+++ LAIGDemoParser20192020v1.1/MyGameBoard.js
@@ -16,10 +16,12 @@ class MyGameBoard{
             
             this.tiles[i] = new MyTile(this.orchestrator, i);
             //this.piece[i] = new MyPiece(this.orchestrator, i);
-            this.truePieces[i]= false;  
+            this.truePieces[i]= false; 
+
+            this.trueCubes[i] = 0; 
            
-            this.trueCubes[i] = 0;
         }
+
     }
     
     picked(id){
@@ -28,17 +30,181 @@ class MyGameBoard{
         this.truePieces[id-1] = true;  
         this.piece[id-1] = new MyPiece(this.orchestrator, id-1, this.turn);
         let i = id -1 ;
-        if(((i%8) && (i>9 && this.truePieces[i-9] && (this.piece[i].getTurn() == this.piece[i-9].getTurn()))) || ((i%8!=7) && i>7 && this.truePieces[i-7] && (this.piece[i].getTurn() == this.piece[i-7].getTurn())) || (this.truePieces[i+9] && (this.piece[i].getTurn() == this.piece[i+9].getTurn())) || (this.truePieces[i+7] && (this.piece[i].getTurn() == this.piece[i+7].getTurn()))){
-            if(this.trueCubes[id-1] == 0){
-                this.cubes[id-1] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
-                this.trueCubes[id-1] = 1;
+
+         /*---------------- [i-9] ----------------------------------------*/
+         if(((i%8) && (i>9 && this.truePieces[i-9] && (this.piece[i].getTurn() == this.piece[i-9].getTurn())))){
+          
+          if(i%8>4 || (i>4 && i<8)){
+                if((i-9)>7.0){
+                    if(this.trueCubes[Math.round((id-8)-((id-8)/8.0))-1] == 0){
+                        this.cubes[Math.round((id-8)-((id-8)/8.0))-1] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[Math.round((id-8)-((id-8)/8.0))-1] = 1;
+
+                    }
+                    else if(this.trueCubes[Math.round((id-8)-((id-8)/8.0))-1] == 1){
+                        this.cubes[Math.round((id-8)-((id-8)/8.0))-1].changeTurn(this.turn);
+                        this.trueCubes[Math.round((id-8)-((id-8)/8.0))-1] == 2;
+                    }
+                }else{
+                    if(this.trueCubes[id-9] == 0){
+                        this.cubes[id-9] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[id-9] = 1;
+
+                    }
+                    else if(this.trueCubes[id-9] == 1){
+                        this.cubes[id-9].changeTurn(this.turn);
+                        this.trueCubes[id-9] == 2;
+                    }
+                }
+            
+         }
+
+         if(i%8<=4 || (i<5 )){
+            if((i-9)>7.0){
+                    if(this.trueCubes[Math.floor((id-8)-((id-8)/8.0))-1] == 0){
+                        this.cubes[Math.floor((id-8)-((id-8)/8.0))-1] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[Math.floor((id-8)-((id-8)/8.0))-1] = 1;
+
+                    }
+                    else if(this.trueCubes[Math.floor((id-8)-((id-8)/8.0))-1] == 1){
+                        this.cubes[Math.floor((id-8)-((id-8)/8.0))-1].changeTurn(this.turn);
+                        this.trueCubes[Math.floor((id-8)-((id-8)/8.0))-1] == 2;
+                    }
+                }else{
+                    if(this.trueCubes[id-9] == 0){
+                        this.cubes[id-9] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[id-9] = 1;
+
+                    }
+                    else if(this.trueCubes[id-9] == 1){
+                        this.cubes[id-9].changeTurn(this.turn);
+                        this.trueCubes[id-9] == 2;
+                    }
+                }
+            }
+         }
+
+        /*---------------- [i-7] ----------------------------------------*/
+        if((i%8!=7.0) && i>7.0 && this.truePieces[i-7.0] && (this.piece[i].getTurn() == this.piece[i-7.0].getTurn())){
+
+            if(i%8>4 || (i>4 && i<8)){
+
+                if((i-7.0)>7.0){
+                    if(this.trueCubes[Math.round((id-8)-((id-8)/8.0))] == 0){
+                        this.cubes[Math.round((id-8)-((id-8)/8.0))] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[Math.round((id-8)-((id-8)/8.0))] = 1;
+
+                    }
+                    else if(this.trueCubes[Math.round((id-8)-((id-8)/8.0))] == 1){
+                        this.cubes[Math.round((id-8)-((id-8)/8.0))].changeTurn(this.turn);
+                        this.trueCubes[Math.round((id-8)-((id-8)/8.0))] == 2;
+                    }
+                }else{
+                    if(this.trueCubes[id-8] == 0){
+                        this.cubes[id-8] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[id-8] = 1;
+
+                    }
+                    else if(this.trueCubes[id-8] == 1){
+                        this.cubes[id-8].changeTurn(this.turn);
+                        this.trueCubes[id-8] == 2;
+                    }
+                }
+
+            } 
+
+            if(i%8<=4 || (i<5)){
+                if((i-7.0)>7.0){
+                    if(this.trueCubes[Math.floor((id-8)-((id-8)/8.0))] == 0){
+                        this.cubes[Math.floor((id-8)-((id-8)/8.0))] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[Math.floor((id-8)-((id-8)/8.0))] = 1;
+
+                    }
+                    else if(this.trueCubes[Math.floor((id-8)-((id-8)/8.0))] == 1){
+                        this.cubes[Math.floor((id-8)-((id-8)/8.0))].changeTurn(this.turn);
+                        this.trueCubes[Math.floor((id-8)-((id-8)/8.0))] == 2;
+                    }
+                }else{
+                    if(this.trueCubes[id-8] == 0){
+                        this.cubes[id-8] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                        this.trueCubes[id-8] = 1;
+
+                    }
+                    else if(this.trueCubes[id-8] == 1){
+                        this.cubes[id-8].changeTurn(this.turn);
+                        this.trueCubes[id-8] == 2;
+                    }
+                }
 
             }
-            else if(this.trueCubes[id-1] == 1){
-                this.cubes[id-1].changeTurn(this.turn);
-                this.trueCubes[id-1] == 2;
+        }
+
+
+        /*---------------- [i+9] ----------------------------------------*/
+        if((this.truePieces[i+9] && (this.piece[i].getTurn() == this.piece[i+9].getTurn())) ){
+
+            if(i%8>4 || (i>4 && i<8)){
+
+                if(this.trueCubes[Math.round(id-(id/8.0))] == 0){
+                this.cubes[Math.round(id-(id/8.0))] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                this.trueCubes[Math.round(id-(id/8.0))] = 1;
+
+            }
+            else if(this.trueCubes[Math.round(id-(id/8.0))] == 1){
+                this.cubes[Math.round(id-(id/8.0))].changeTurn(this.turn);
+                this.trueCubes[Math.round(id-(id/8.0))] == 2;
+            }
+            }
+
+            if(i%8<=4 || (i<5)){
+
+                if(this.trueCubes[Math.floor(id-(id/8.0))] == 0){
+                this.cubes[Math.floor(id-(id/8.0))] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                this.trueCubes[Math.floor(id-(id/8.0))] = 1;
+
+            }
+            else if(this.trueCubes[Math.floor(id-(id/8.0))] == 1){
+                this.cubes[Math.floor(id-(id/8.0))].changeTurn(this.turn);
+                this.trueCubes[Math.floor(id-(id/8.0))] == 2;
+            }
+            }
+
+            
+
+        }
+
+
+        /*---------------- [i+7] ----------------------------------------*/
+        if((this.truePieces[i+7.0] && (this.piece[i].getTurn() == this.piece[i+7.0].getTurn()))){
+
+            if(i%8>4 || (i>4 && i<8)){
+
+                if(this.trueCubes[Math.round(id-(id/8.0)-1)] == 0){
+                    this.cubes[Math.round(id-(id/8.0)-1)] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                    this.trueCubes[Math.round(id-(id/8.0)-1)] = 1;
+
+                }
+                else if(this.trueCubes[Math.round(id-(id/8.0)-1)] == 1){
+                    this.cubes[Math.round(id-(id/8.0)-1)].changeTurn(this.turn);
+                    this.trueCubes[Math.round(id-(id/8.0)-1)] == 2;
+                }
+            }
+
+            if(i%8<=4 || (i<5)){
+
+                if(this.trueCubes[Math.floor(id-(id/8.0)-1)] == 0){
+                    this.cubes[Math.floor(id-(id/8.0)-1)] = new MyUnitCubeQuad(this.orchestrator.getScene(), this.turn);
+                    this.trueCubes[Math.floor(id-(id/8.0)-1)] = 1;
+
+                }
+                else if(this.trueCubes[Math.floor(id-(id/8.0)-1)] == 1){
+                    this.cubes[Math.floor(id-(id/8.0)-1)].changeTurn(this.turn);
+                    this.trueCubes[Math.floor(id-(id/8.0)-1)] == 2;
+                }
             }
         }
+
+
         setTimeout(() => { this.turn = Math.abs(this.turn - 1); this.orchestrator.getScene().setPickEnabled(true); }, 1000); 
         if(this.turn){
             
@@ -51,7 +217,7 @@ class MyGameBoard{
     }
     display(){
         this.orchestrator.getScene().logPicking();
-        this.orchestrator.getScene().clearPickRegistration();
+        this.orchestrator.getScene().clearPickRegistration();;
        
         for (let i = 0; i < this.tiles.length; i++) {
             
@@ -62,16 +228,34 @@ class MyGameBoard{
                 this.orchestrator.getScene().popMatrix();
                 this.orchestrator.getScene().pushMatrix();
                 
-                this.piece[i].display(i);
-                if(this.cubes[i]!=undefined){
-                    this.orchestrator.getScene().rotate(-Math.PI/2-Math.PI/8, 0, 0, 1);                    
-                    this.orchestrator.getScene().scale(0.45, 0.45, 0.2);
-                    this.orchestrator.getScene().translate(1.55, 0, 0.5);
-                    this.cubes[i].display();
-                    this.orchestrator.getScene().translate(-1.55, 0, -0.5);
-                    this.orchestrator.getScene().scale(2.5, 2.5, 5);
-                    this.orchestrator.getScene().rotate(-(-Math.PI/2-Math.PI/8), 0, 0, 1); 
+                this.piece[i].display(i); 
+            }
+
+            if(i%8>4 || (i>4 && i<8)){
+
+                if(this.cubes[Math.round(i-(i/8.0))]!=undefined && i<55 && i!=0 && (i%8)-1!=0){
+                        this.orchestrator.getScene().rotate(-Math.PI/2-Math.PI/8, 0, 0, 1);                    
+                        //this.orchestrator.getScene().rotate(-Math.PI, 0, 0, 1);
+                        this.orchestrator.getScene().scale(0.45, 0.45, 0.2);
+                        this.orchestrator.getScene().translate(1.55, 0, 0.5);
+                        this.cubes[Math.round(i-(i/8.0))].display();
+                        this.orchestrator.getScene().translate(-1.55, 0, -0.5);
+                        this.orchestrator.getScene().scale(2.5, 2.5, 5);
+                        this.orchestrator.getScene().rotate(-(-Math.PI/2-Math.PI/8), 0, 0, 1);
                 }
+            }else if(i%8<=4 || (i<5)){
+                if(this.cubes[Math.floor(i-(i/8.0))]!=undefined && i<55 && i!=0 && (i%8)-1!=0){
+                        this.orchestrator.getScene().rotate(-Math.PI/2-Math.PI/8, 0, 0, 1);                    
+                        //this.orchestrator.getScene().rotate(-Math.PI, 0, 0, 1);
+                        this.orchestrator.getScene().scale(0.45, 0.45, 0.2);
+                        this.orchestrator.getScene().translate(1.55, 0, 0.5);
+                        this.cubes[Math.floor(i-(i/8.0))].display();
+                        this.orchestrator.getScene().translate(-1.55, 0, -0.5);
+                        this.orchestrator.getScene().scale(2.5, 2.5, 5);
+                        this.orchestrator.getScene().rotate(-(-Math.PI/2-Math.PI/8), 0, 0, 1);
+            }
+        }
+
             //     if((i%8) && (i>9 && this.truePieces[i-9] && (this.piece[i].getTurn() == this.piece[i-9].getTurn()))){
             //         // if (this.trueCubes[i] == 1) {
             //         //     this.cubes[i].changeTurn(this.turn);
@@ -91,7 +275,7 @@ class MyGameBoard{
             //             this.orchestrator.getScene().rotate(Math.PI/8, 0, 0, 1); 
             //         //}
             //     }
-            //     if((i%8!=7) && i>7 && this.truePieces[i-7] && (this.piece[i].getTurn() == this.piece[i-7].getTurn())){
+            //     if((i%8!=7.0) && i>7.0 && this.truePieces[i-7.0] && (this.piece[i].getTurn() == this.piece[i-7.0].getTurn())){
             //         // if (this.trueCubes[i+1] == 0) {
             //         //     this.cubes[i+1].changeTurn(this.turn);
             //         //     this.trueCubes[i+1] = 3;
@@ -109,14 +293,10 @@ class MyGameBoard{
             //         this.orchestrator.getScene().rotate(-(-Math.PI/2-Math.PI/8), 0, 0, 1); 
             //     //}
             // }
-            }
-            
-        } 
-
-
 
        // this.tile1.display();
     }
+}
 
     undo(id){
         this.orchestrator.getScene().setPickEnabled(false);
